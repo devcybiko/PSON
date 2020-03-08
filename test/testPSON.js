be481bc50e38ddb4c769e6b7f9d50919a40b46c8
Greg Smith <thinkable@thinkable.us>
2020-03-08 01:10:01 -0500

    automatic commit

--- test/testPSON.js
+++ test/testPSON.js
@@ -3,20 +3,20 @@ const gls = require('glstools').files;
 
 function test_files() {
     let files = gls.readDir('.', file => file.endsWith(".pson"));
-    for(let file of files) {
-        let text = gls.read(file).trim();
-        let obj = PSON.parse(text);
+    for(let psonFile of files) {
+        let psonText = gls.read(psonFile).trim();
+        let obj = PSON.parse(psonText);
         let pson = JSON.stringify(obj, null, 2).trim();
-        let jsonFile = file.replace(".pson", ".json");
+        let jsonFile = psonFile.replace(".pson", ".json");
         let json = gls.read(jsonFile).trim();
         for(let i=0; i<json.length; i++) {
             if (json[i] !== pson[i]) {
-                console.error(`ERROR-NO-MATCH ${i} ${json[i]} ${pson[i]}`);
+                console.error(`ERROR-NO-MATCH char:${i} pson:${pson[i]} json:${json[i]}`);
                 break;
             }
         }
-        if (json !== text) {
-            console.error(`ERROR: ${file} did not match ${jsonFile}`);
+        if (json !== pson) {
+            console.error(`ERROR: ${psonFile} did not match ${jsonFile}`);
             break;
         }
     }
